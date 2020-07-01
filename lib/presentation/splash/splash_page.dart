6046c2bdfa39f75b9556e1a9aab9c2bb96af13be
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-01 11:42:31 +0100

    lib/presentation/splash/splash_page

---
+++ lib/presentation/splash/splash_page.dart
@@ -0,0 +1,29 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:ddd/application/auth/auth_bloc.dart';
+import 'package:ddd/presentation/routes/router.gr.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        state.map(
+          initial: (_) {},
+          authenticated: (_) {
+            print("I am authenticated!");
+          }, // ExtendedNavigator.of(context).pushReplacementNamed(),
+          unauthenticated: (_) => ExtendedNavigator.of(context)
+              .pushReplacementNamed(Routes.signInPage),
+        );
+      },
+      child: const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
