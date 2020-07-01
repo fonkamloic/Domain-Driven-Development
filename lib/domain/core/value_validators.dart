992a6da5e0ce00774551b99dd008a2e69d39f05a
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-01 14:36:54 +0100

    lib/domain/core/value_validators

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,8 +1,43 @@
 import 'package:dartz/dartz.dart';
-import 'package:ddd/domain/auth/valid_objects.dart';
 import 'package:ddd/domain/core/failures.dart';
+import 'package:kt_dart/kt.dart';
 
-Either<ValueFailure<String>, String>  validateEmailAddress(String input) {
+Either<ValueFailure<String>, String> validateMaxStringLength(
+    String input, int maxLength) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(
+        ValueFailure.exceedingLength(failedValue: input, max: maxLength));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (input.contains('\n')) {
+    return left(ValueFailure.multiline(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+    KtList<T> input, int maxLength) {
+  if (input.size <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.listTooLong(max: maxLength, failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const emailRegex =
       r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
   if (RegExp(emailRegex).hasMatch(input)) {
@@ -12,7 +47,7 @@ Either<ValueFailure<String>, String>  validateEmailAddress(String input) {
   }
 }
 
-Either<ValueFailure<String>, String>  validatePassword(String input) {
+Either<ValueFailure<String>, String> validatePassword(String input) {
   if (input.length > 5) {
     return right(input);
   } else {
