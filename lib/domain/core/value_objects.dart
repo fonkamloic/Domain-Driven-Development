8b3f01a18bf8ff52c0a9a662cea2649855d7bca9
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-01 16:26:05 +0100

    lib/domain/core/value_objects

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -13,6 +13,10 @@ abstract class ValueObject<T> {
 
   bool isValid() => value.isRight();
 
+  Either<ValueFailure<dynamic>, Unit> get failureOrUnit {
+    return value.fold((l) => left(l), (_) => right(unit));
+  }
+
   /// Throws [UnexpectedValueError] containing the [ValueFailutrfd]
   T getOrCrash() {
     //id = idendity = same as writing (right)  => right;
