d9dd1b3c10d24f85f5a207f311d646fa9f89935a
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-01 10:17:21 +0100

    lib/main

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:ddd/injections.dart';
+import 'package:ddd/presentation/core/app_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+
+void main() {
+  configureInjection(Environment.prod);
+  runApp(MyApp());
+}
+
