720af5908390acf02c5ccd43dccab248aaf85fd8
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-01 16:26:05 +0100

    lib/domain/notes/todo_item

---
+++ lib/domain/notes/todo_item.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd/domain/core/failures.dart';
+import 'package:ddd/domain/core/value_objects.dart';
+import 'package:ddd/domain/notes/value_objects.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo_item.freezed.dart';
+
+@freezed
+abstract class TodoItem implements _$TodoItem {
+  const TodoItem._();
+
+  const factory TodoItem({
+    @required TodoName name,
+    @required UniqueId id,
+    @required bool done,
+  }) = _TodoName;
+
+  factory TodoItem.empty() => TodoItem(
+        id: UniqueId(),
+        name: TodoName(''),
+        done: false,
+      );
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return name.value.fold((f) => some(f), (_) => none());
+  }
+}
