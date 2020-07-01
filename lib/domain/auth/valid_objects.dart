1d271945a68a92ec594e87a8aabed0816c7259c5
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-01 10:17:20 +0100

    lib/domain/auth/valid_objects

---
+++ lib/domain/auth/valid_objects.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd/domain/core/failures.dart';
+import 'package:ddd/domain/core/value_objects.dart';
+import 'package:ddd/domain/core/value_validators.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+
+@immutable
+class EmailAddress extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(validateEmailAddress(input));
+  }
+
+  const EmailAddress._(this.value);
+}
+
+
+@immutable
+class Password extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(validatePassword(input));
+  }
+
+  const Password._(this.value);
+}
+
+
