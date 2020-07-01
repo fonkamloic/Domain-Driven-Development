dc53060d9e6d755480068cb18139927d96881f9e
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-01 14:36:54 +0100

    lib/infractructure/auth/firebase_auth_facade

--- lib/infractructure/auth/firebase_auth_facade.dart
+++ lib/infractructure/auth/firebase_auth_facade.dart
@@ -10,9 +10,7 @@ import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 
-@lazySingleton
-// @Injectable(as: IAuthFacade)
-@RegisterAs(IAuthFacade)
+@LazySingleton(as: IAuthFacade)
 class FirebaseAuthFacade implements IAuthFacade {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
