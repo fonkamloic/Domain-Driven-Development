345f2003646f214970f5ff1d71d63247e28ca31a
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-02 14:26:30 +0100

    lib/injections.iconfig

--- lib/injections.iconfig.dart
+++ lib/injections.iconfig.dart
@@ -9,6 +9,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ddd/infractructure/auth/firebase_auth_facade.dart';
 import 'package:ddd/domain/auth/i_auth_facade.dart';
+import 'package:ddd/application/notes/note_actor_bloc/note_actor_bloc.dart';
+import 'package:ddd/domain/notes/i_note_repository.dart';
+import 'package:ddd/application/notes/note_watcher_bloc/note_watcher_bloc.dart';
 import 'package:ddd/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:ddd/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -21,6 +24,9 @@ void $initGetIt(GetIt g, {String environment}) {
       () => firebaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerFactory<NoteActorBloc>(() => NoteActorBloc(g<INoteRepository>()));
+  g.registerFactory<NoteWatcherBloc>(
+      () => NoteWatcherBloc(g<INoteRepository>()));
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
   g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
 }
