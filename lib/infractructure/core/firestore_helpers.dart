bb01be75f4c7c4c68ba12924f5d999a1226c3885
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-04 01:01:59 +0100

    lib/infractructure/core/firestore_helpers

---
+++ lib/infractructure/core/firestore_helpers.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ddd/domain/auth/i_auth_facade.dart';
+import 'package:ddd/domain/core/errors.dart';
+import 'package:ddd/injections.dart';
+
+extension FirestoreX on Firestore {
+  Future<DocumentReference> userDocument() async {
+    final userOption = await getIt<IAuthFacade>().getSignedInUser();
+    final user = userOption.getOrElse(() => throw NotAuthenticatedError());
+    return Firestore.instance
+        .collection('users')
+        .document(user.id.getOrCrash());
+  }
+}
+
+extension DocumentReferenceX on DocumentReference {
+  CollectionReference get noteCollection => collection('notes');
+}
