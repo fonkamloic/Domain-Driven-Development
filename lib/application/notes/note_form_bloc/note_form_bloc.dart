a8332470950b96e35344dc08aa2a3aaae678afcf
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-05 23:30:15 +0100

    lib/application/notes/note_form_bloc/note_form_bloc

---
+++ lib/application/notes/note_form_bloc/note_form_bloc.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ddd/domain/notes/i_note_repository.dart';
+import 'package:ddd/domain/notes/note.dart';
+import 'package:ddd/domain/notes/note_failure.dart';
+import 'package:ddd/domain/notes/value_objects.dart';
+import 'package:ddd/presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:meta/meta.dart';
+
+part 'note_form_event.dart';
+part 'note_form_state.dart';
+
+part 'note_form_bloc.freezed.dart';
+
+@injectable
+class NoteFormBloc extends Bloc<NoteFormEvent, NoteFormState> {
+  final INoteRepository _noteRepository;
+
+  NoteFormBloc(this._noteRepository);
+
+  @override
+  NoteFormState get initialState => NoteFormState.initial();
+
+  @override
+  Stream<NoteFormState> mapEventToState(
+    NoteFormEvent event,
+  ) async* {
+    yield* event.map(
+      initialized: (e) async* {
+        yield e.initialNoteOption.fold(
+            () => state,
+            (initailNote) => state.copyWith(
+                  note: initailNote,
+                  isEditing: true,
+                ));
+      },
+      bodyChanged: (e) async* {
+        yield state.copyWith(
+          note: state.note.copyWith(body: NoteBody(e.bodyStr)),
+          saveFailureOrSuccessOption: none(),
+        );
+      },
+      colorChanged: (e) async* {
+        yield state.copyWith(
+          note: state.note.copyWith(color: NoteColor(e.color)),
+          saveFailureOrSuccessOption: none(),
+        );
+      },
+      todosChanged: (e) async* {
+        yield state.copyWith(
+          note: state.note.copyWith(
+            todos: List3(e.todoList.map((primitive) => primitive.toDomain())),
+          ),
+          saveFailureOrSuccessOption: none(),
+        );
+      },
+      saved: (e) async* {
+        Either<NoteFailure, Unit> failureOrsuccess;
+        yield state.copyWith(
+          isSaving: true,
+          saveFailureOrSuccessOption: none(),
+        );
+
+        if (state.note.failureOption.isNone()) {
+          failureOrsuccess = state.isEditing
+              ? await _noteRepository.update(state.note)
+              : await _noteRepository.create(state.note);
+        }
+        yield state.copyWith(
+          isSaving: false,
+          showErrorMessage: true,
+          saveFailureOrSuccessOption: optionOf(failureOrsuccess),
+        );
+      },
+    );
+  }
+}
