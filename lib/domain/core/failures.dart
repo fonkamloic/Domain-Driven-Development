ea4f9ce64e5c2a38f798c1386a3eaed1ce04c03f
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-30 21:11:04 +0100

    lib/domain/core/failures

--- lib/domain/core/failures.dart
+++ lib/domain/core/failures.dart
@@ -2,22 +2,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'failures.freezed.dart';
 
-// @freezed
-// abstract class ValueFailure<T> with _$ValueFailure<T> {
-//   const factory ValueFailure.auth(AuthValueFailure<T> f) = _Auth<T>;
-//   const factory ValueFailure.notes(NotesValueFailure<T> f) = _Notes<T>;
 
-// }
-
-// @freezed
-// abstract class NotesValueFailure<T> with _$NotesValueFailure<T> {
-//   const factory NotesValueFailure.exceedingLength({
-//     @required String failedValue,
-//   }) = ExceedingLength<T>;
-//   // const factory ValueFailure.shortPassword({
-//   //   @required String failedValue,
-//   // }) = ShortPassword<T>;
-// }
 
 @freezed
 abstract class ValueFailure<T> with _$ValueFailure<T> {
