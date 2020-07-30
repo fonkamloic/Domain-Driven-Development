389c609297d8e90278b9693113a34c37dbcdbb71
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-30 21:11:05 +0100

    lib/main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:ddd/application/simple_bloc_delegate.dart';
 import 'package:ddd/injections.dart';
 import 'package:ddd/presentation/core/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+
 
 void main() {
   configureInjection(Environment.prod);
+  /** BlocDelegate allows us to override onTransition and onError and will help us see all bloc state changes (transitions) and errors in one place!
+ */
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(AppWidget());
 }
 
