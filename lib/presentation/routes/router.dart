5a614277f32e14d8330afa0b5130e0562a000b6f
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-30 21:11:06 +0100

    lib/presentation/routes/router

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,10 +1,13 @@
 import 'package:auto_route/auto_route_annotations.dart';
+import 'package:ddd/presentation/notes/notes_overview/notes_overview_page.dart';
 import 'package:ddd/presentation/sign_in/sign_in.dart';
 import 'package:ddd/presentation/splash/splash_page.dart';
 
-@MaterialAutoRouter(generateNavigationHelperExtension: true)
-class $Router {
-  @initial
-  SplashPage splashPage;
-  SignInPage signInPage;
-}
+@MaterialAutoRouter(
+    generateNavigationHelperExtension: true,
+    routes: <AutoRoute>[
+      MaterialRoute(page: SplashPage, initial: true),
+      MaterialRoute(page: SignInPage),
+      MaterialRoute(page: NoteOverviewPage),
+    ])
+class $Router {}
