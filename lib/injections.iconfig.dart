bb2d03ce8f4ab387efbad30ba975485a1746be7e
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-30 21:11:05 +0100

    lib/injections.iconfig

--- lib/injections.iconfig.dart
+++ lib/injections.iconfig.dart
@@ -6,12 +6,12 @@
 
 import 'package:ddd/infractructure/core/firebase_injectable_module.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ddd/infractructure/auth/firebase_auth_facade.dart';
 import 'package:ddd/domain/auth/i_auth_facade.dart';
 import 'package:ddd/infractructure/notes/note_repository.dart';
 import 'package:ddd/domain/notes/i_note_repository.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ddd/application/notes/note_actor_bloc/note_actor_bloc.dart';
 import 'package:ddd/application/notes/note_form_bloc/note_form_bloc.dart';
 import 'package:ddd/application/notes/note_watcher_bloc/note_watcher_bloc.dart';
@@ -23,6 +23,7 @@ void $initGetIt(GetIt g, {String environment}) {
   final firebaseInjectableModule = _$FirebaseInjectableModule();
   g.registerLazySingleton<FirebaseAuth>(
       () => firebaseInjectableModule.firebaseAuth);
+  g.registerLazySingleton<Firestore>(() => firebaseInjectableModule.firestore);
   g.registerLazySingleton<GoogleSignIn>(
       () => firebaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
