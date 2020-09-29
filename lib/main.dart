81223e20c7379e6b93c4148a525c08bec0ae598f
Fonkam Loïc <fonkamloic@gmail.com>
2020-09-30 00:35:51 +0100

    lib/main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:ddd/application/simple_bloc_delegate.dart';
 import 'package:ddd/injections.dart';
 import 'package:ddd/presentation/core/app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
-
-void main() {
-  configureInjection(Environment.prod);
-  /** BlocDelegate allows us to override onTransition and onError and will help us see all bloc state changes (transitions) and errors in one place!
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+ await configureInjection(Environment.prod);
+ /** BlocDelegate allows us to override onTransition and onError and will help us see all bloc state changes (transitions) and errors in one place!
  */
-  runApp(AppWidget());
+  Bloc.observer = MyBlocObserver();
+
+  await Firebase.initializeApp();
+  runApp(const AppWidget());
   // BlocSupervisor.delegate = SimpleBlocDelegate();
 }
-
